911d09d843f8519d2591fdc6504b2c1fce7459a3
dollarkillerx <dollarkiller@vip.qq.com>
2019-01-23 14:52:29 +0800

    update

--- TEST/主从同步/donation.php
+++ TEST/主从同步/donation.php
@@ -6,3 +6,37 @@ header('content-type:text/html;charset=utf8');
  * Date: 2019/1/21
  * Time: 下午 09:31
  */
+$cache = new lib\Memcache\MemCache();
+
+$id = intval($_GET['id']);
+
+//查询转赠记录表
+
+//查询记录
+$where = [
+  'id'=>$id,
+  'status'=>1,//未被转赠
+];
+
+//查询结果
+$result = true;
+
+if ($result){
+    //其他人已经领取了主从未同步的情况
+    if ($cache->get("donation:{$id}")) {
+        echo '已被领取';
+    }else{
+        if ($cache->add("donation:{$id}",true,1200)){
+            $update = [
+                'to_user_id'=>'b',
+                'status'=>2,
+                'donation_time'=>$_SERVER['REQUEST_TIME']
+            ];
+            echo '恭喜你领取成功';
+        }else{
+            echo '已经被领取';
+        }
+    }
+}else{
+    echo '已被领取';
+}
--- TEST/主从同步/index.php
+++ TEST/主从同步/index.php
@@ -7,7 +7,18 @@ header('content-type:text/html;charset=utf8');
  * Time: 下午 09:31
  */
 
-$donation = [
-  'user_id' => 1,
+//用户设置转赠
 
+
+$donation = [
+    'user_id' => 1,
+    'integral'=> 30,
+    'time' => $_SERVER['REQUEST_TIME'],
+    'status' => 1
 ];
+
+//写入数据库转正表
+$donationId = 123;
+
+//donation.php?id=123 控制器
+
